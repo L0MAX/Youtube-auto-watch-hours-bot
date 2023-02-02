f8c90c57a8e87967d4a905fdf88edece4a7dcb33
L0MAX <ankahdonatus@gmail.com>
2023-02-02 10:45:50 +0000

    created a youtube watch hours bot

---
+++ README.md
@@ -0,0 +1,34 @@
+# Youtube auto watch bot
+
+> This is a bot created in python to generate watch time and views for YouTube channel.
+> I create this project for educational purposes, you can use it to generate views and increase watch time, but this is not the original intention.
+> you can use, copy, modify this code as you like.
+
+### How to use:
+
+#### Install
+
+- sudo apt install chromium-chromedriver
+- sudo apt install python3-pip
+- pip3 install -r requirment.txt
+
+#### usage
+
+- python3 youtubeBot.py
+- enter the URL of your video
+- enter how many times you want the video to be watched
+- enter the length of the video or how many seconds you want the video to be watched each time
+
+### Built with
+
+- Python`
+
+## Author
+
+- [Twitter](https://www.twitter.com/ismailcourr)
+- [Github](https://github.com/L0MAX)
+- [Linkedin](https://www.linkedin.com/in/ismailcourr/)
+
+## Show your support
+
+Give a ⭐️ if you like this project!
---
+++ youtubeBot.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+from time import sleep
+import random
+
+# get the proxies from proxy.txt and store it into proxy_list array
+proxy_list = []
+
+with open('proxy.txt') as fil:
+    proxy_list = [i.strip() for i in fil]
+
+# get input from user
+URL = input("enter youtube video URL ...\n")
+timeToReopenBrowser = input("Reopen the browser n times ...\n")
+videoLength = input("How long is the video?\n")
+
+# convert input to int
+timeToReopenBrowser = int(timeToReopenBrowser)
+videoLength = int(videoLength)
+
+# get a rendom proxy every time the while loop runs
+# open chrome and go to the URL link
+# wait for n secounds to close the browser and open it again
+i = 0
+while i < timeToReopenBrowser:
+    proxies = random.choice(proxy_list)
+    proxy_ip_port = proxies
+
+    print("The proxy used is:", proxy_ip_port)
+
+    # set proxy
+
+    proxy = Proxy()
+    proxy.proxy_type = ProxyType.MANUAL
+    proxy.http_proxy = proxy_ip_port
+    proxy.ssl_proxy = proxy_ip_port
+
+    capabilities = webdriver.DesiredCapabilities.CHROME
+    proxy.add_to_capabilities(capabilities)
+
+    # let's call the browser
+
+    driver = webdriver.Chrome(desired_capabilities=capabilities)
+    driver.get(URL)
+
+    sleep(videoLength)
+
+    # close browser
+
+    driver.quit()
+
+    i += 1
+
+# https://openproxy.space/api replace static proxies
